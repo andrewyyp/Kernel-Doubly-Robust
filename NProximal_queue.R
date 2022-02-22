8fc4420e714a802c03a8b8c8147486d79919fe0e
Andrew Ying <33556113+andrewyyp@users.noreply.github.com>
2022-02-22 10:01:57 -0800

    Update NProximal_queue.R

--- NProximal_queue.R
+++ NProximal_queue.R
@@ -1,6 +1,3 @@
-#rm(list = ls())
-#begin <- Sys.time()
-#library(ggplot2)
 library(caret)
 library(corpcor)
 library(rdetools)
@@ -8,10 +5,6 @@ library(expm)
 library(MASS)
 source("DGP2.R")
 source("NEval.R")
-
-
-arg <- commandArgs(trailingOnly = TRUE)
-njob <- eval(parse(text = arg[[1]]))
 
 dim_x <- 5
 dim_z <- 2
@@ -28,9 +21,6 @@ N <- 400
 
 
 
-# Fres_OR <- c()
-# Fres_IPW <- c()
-# Fres_DR <- c()
 
 ## candidates of hyperparameters
 lm_Hh1_list <- c(0.0001, 0.001, 0.01, 0.1)
@@ -53,14 +43,7 @@ result <- CV_eval(data, kernel_sigma_list, theta = 0, lm_Hh1_list, lm_Qh1_list, 
 
 kernel_sigma <- result$kernel_sigma
 hyperparameters <- result$hyperparameters
-# lm_Hh1 <- 0.0002
-# lm_Qh1 <- 50
-# lm_Hh0 <- 0.0002
-# lm_Qh0 <- 50
-# lm_Hq1 <- 50
-# lm_Qq1 <- 0.0002
-# lm_Hq0 <- 50
-# lm_Qq0 <- 0.0002
+
 
 
 lm_Hh1 <- hyperparameters[1]
@@ -73,13 +56,7 @@ lm_Hq0 <- hyperparameters[7]
 lm_Qq0 <- hyperparameters[8]
 ## use cross fitting to get the empirical bias
 est <- CF_eval(data, kernel_sigma, lm_Hh1, lm_Qh1, lm_Hh0, lm_Qh0, lm_Hq1, lm_Qq1, lm_Hq0, lm_Qq0, CF_K_fold, Nystroem = TRUE, appro_rate = 0.05)
-# est <- insample_eval(data, kernel_sigma, lm_Hh1, lm_Qh1, lm_Hh0, lm_Qh0, lm_Hq1, lm_Qq1, lm_Hq0,  Nystroem = TRUE, appro_rate = 0.05)
 est <- Re(est)
-# print(Sys.time() - begin)
-# begin <- Sys.time()
-# est <- insample_eval(data, kernel_sigma, lm_Hh1, lm_Qh1, lm_Hh0, lm_Qh0, lm_Hq1, lm_Qq1, lm_Hq0)
-# est
-#print(Sys.time() - begin)
 
 write.csv(est, file= paste0("Res/", type, "_", N, "_", njob,".csv"), row.names = FALSE)
 
